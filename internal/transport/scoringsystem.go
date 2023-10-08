c8fd9280eea638077b8e73a9895d94b38c1161a9
AlexCorn999 <alexcorn999@list.ru>
2023-10-08 11:19:09 +0300

    checking scor system

--- internal/transport/scoringsystem.go
+++ internal/transport/scoringsystem.go
@@ -22,8 +22,25 @@ func (s *APIServer) ScoringSystem() {
 		return
 	}
 
+	fmt.Println("Response status code:", resp.StatusCode)
 	fmt.Println("Response body:", string(body))
 
+	addr2 := fmt.Sprintf("%s/api/orders/20412011", s.config.ScoringSystemPort)
+	resp2, err := http.Get(addr2)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+	defer resp2.Body.Close()
+
+	body2, err := io.ReadAll(resp2.Body)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+
+	fmt.Println("Response status code:", resp2.StatusCode)
+	fmt.Println("Response body:", string(body2))
 	// берем заказ из системы если его нету то ошибка 204 заказа нет в системе
 
 	// если заказ есть то делаем гет запрос
